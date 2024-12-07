e0af6479696e96b3b2be649934f858ab45906c10
Andreas <andreas.vralstad@gmail.com>
2024-12-07 13:53:01 +0100

    Day 4, part 2

--- aoc.py
+++ aoc.py
@@ -18,7 +18,7 @@ def read_file_as_single_line(fn):
     return s, num_lines, line_length
 
 
-def day4(fn):
+def day4_part1(fn):
     input, num_lines, line_length = read_file_as_single_line(fn)
     all_positions = all_occurences_in_string(input, "X")
     word_to_match = "XMAS"
@@ -61,14 +61,33 @@ def day4(fn):
     return num_occurences
 
 
+def day4_part2(fn):
+    input, num_lines, line_length = read_file_as_single_line(fn)
+    all_positions = all_occurences_in_string(input, "A")
+    num_occurences = 0
+    for pos in all_positions:
+        row = pos // line_length
+        col = pos % line_length
+        ok_row = row >= 1 and row < num_lines - 1
+        ok_col = col >= 1 and col < line_length - 1
+        if ok_row and ok_col:
+            s1 = input[pos - line_length - 1] + input[pos + line_length + 1]
+            s2 = input[pos - line_length + 1] + input[pos + line_length - 1]
+            if (s1 == "MS" or s1 == "SM") and (s2 == "MS" or s2 == "SM"):
+                num_occurences += 1
+    return num_occurences
+
+
 def aoc_2024():
-    days = {"Day4": day4}
+    days = {"Day4": [day4_part1, day4_part2]}
     for name in days:
         fn_test = f"input/{name}-test.txt"
         fn_real = f"input/{name}-real.txt"
-        res_test = days[name](fn_test)
-        res_real = days[name](fn_real)
-        print(f"{name}: {res_test} / {res_real}")
+        parts = days[name]
+        for idx, func in enumerate(parts):
+            res_test = func(fn_test)
+            res_real = func(fn_real)
+            print(f"{name} (part {idx+1}): {res_test} / {res_real}")
 
 
 if __name__ == "__main__":
